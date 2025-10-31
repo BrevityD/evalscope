46966e3b049a1b5cec539fe17c91ed9d4db4bb3e
dzj <brevity_d@mail.ustc.edu.cn>
2025-10-31 10:54:53 +0800

    fix neradapter

--- evalscope/api/benchmark/__init__.py
+++ evalscope/api/benchmark/__init__.py
@@ -1,3 +1,3 @@
-from .adapters import DefaultDataAdapter, ImageEditAdapter, MultiChoiceAdapter, WOChoiceMultiChoiceAdapter, Text2ImageAdapter, VisionLanguageAdapter
+from .adapters import DefaultDataAdapter, ImageEditAdapter, MultiChoiceAdapter, NERAdapter, WOChoiceMultiChoiceAdapter, Text2ImageAdapter, VisionLanguageAdapter
 from .benchmark import DataAdapter
 from .meta import BenchmarkMeta
